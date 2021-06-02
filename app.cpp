886b67a7aab9ab8afb8cd5387a34529c51f5031b
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-02 17:40:11 +0530

    update main

--- app.cpp
+++ app.cpp
@@ -46,5 +46,33 @@ public:
 
 int main()
 {
+    Bank b;
+    Account acc;
+
+    int choice;
+    string fname, lname;
+    long accountNumber;
+    float balance;
+    float amount;
+    cout << "******Banking System******" << endl;
+
+    do
+    {
+        cout << "\n\t Select one option";
+        cout << "\n\t 1. Open an Account";
+        cout << "\n\t 2. Balance Enquiry";
+        cout << "\n\t 3. Deposit";
+        cout << "\n\t 4. Withdrawal";
+        cout << "\n\t 5. Close an Account";
+        cout << "\n\t 6. Show All Accounts";
+        cout << "\n\t 7. Quit";
+        cout << "\n\n\t Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        }
+    } while ();
+
     return 0;
 }
