d0233fd1f9f29a6c8508860bd5dec32828ed231f
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-02 17:24:28 +0530

    Bank class prototype

--- app.cpp
+++ app.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+class Bank
+{
+private:
+    map<long, Account> accounts;
+
+public:
+    Bank();
+    Account OpenAccount(string fname, string lname, float balance);
+    Account BalanceEnquiry(long accountNumber);
+    Account Deposit(long accountNumber, float amount);
+    Account Withdraw(long accountNumber, float amount);
+    void CloseAccount(long accountNumber);
+    void ShowAllAccounts();
+    ~Bank();
+};
+
 int main()
 {
     return 0;
