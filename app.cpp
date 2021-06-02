4392ec44b4c6e887ea4c1dd1983ff4aa7ca4f7a0
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-02 17:33:59 +0530

    setup Account Class

--- app.cpp
+++ app.cpp
@@ -2,6 +2,32 @@
 
 using namespace std;
 
+class Account
+{
+private:
+    long accountNumber;
+    string firstName;
+    string lastName;
+    float balance;
+    static long NextAccountNumber;
+
+public:
+    Account() {}
+    Account(string fname, string lname, float balance);
+    long getAccNo() { return accountNumber; }
+    string getFirstName() { return firstName; }
+    string getLastName() { return lastName; }
+    float getBalance() { return balance; }
+
+    void Deposit(float amount);
+    void WithDraw(float amount);
+    static void setLastAccountNumber(long accountNumber);
+    static long getLastAccountNumber();
+    friend ofstream &operator<<(ofstream &ofs, Account &acc);
+    friend ifstream &operator>>(ifstream &ifs, Account &acc);
+    friend ostream &operator<<(ostream &os, Account &acc);
+};
+
 class Bank
 {
 private:
