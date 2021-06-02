107dfe62db93f6149a572f7e1c0e89cd15489ae5
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-02 18:20:29 +0530

    defined Account class functions

--- app.cpp
+++ app.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <map>
 
 using namespace std;
+
+#define MIN_BALANCE 500
+class InsufficientFunds
+{
+};
 
 class Account
 {
@@ -136,4 +142,48 @@ Account::Account(string fname, string lname, float balance)
     firstName = fname;
     lastName = lname;
     this->balance = balance;
+}
+void Account::Deposit(float amount)
+{
+    balance += amount;
+}
+void Account::WithDraw(float amount)
+{
+    if (balance - amount < MIN_BALANCE)
+    {
+        throw InsufficientFund();
+    }
+    balance -= amount;
+}
+void Account::setLastAccountNumber(long accountNumber)
+{
+    NextAccountNumber = accountNumber;
+}
+long Account::getLastAccountNumber()
+{
+    return NextAccountNumber;
+}
+ofstream &operator<<(ofstream &ofs, Account &acc)
+{
+    ofs << acc.accountNumber << endl;
+    ofs << acc.firstName << endl;
+    ofs << acc.lastName << endl;
+    ofs << acc.balance << endl;
+    return ofs;
+}
+ifstream &operator>>(ifstream &ifs, Account &acc)
+{
+    ifs >> acc.accountNumber;
+    ifs >> acc.firstName;
+    ifs >> acc.lastName;
+    ifs >> acc.balance;
+    return ifs;
+}
+ostream &operator<<(ostream &os, Account &acc)
+{
+    os << "First Name: " << acc.getFirstName() << endl;
+    os << "Last Name: " << acc.getLastName() << endl;
+    os << "Account Number: " << acc.getAccNo() << endl;
+    os << "Balance: " << acc.getBalance() << endl;
+    return os;
 }
